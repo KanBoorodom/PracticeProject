c197332c8f39ec1c5b32ad1d96e03b923227aa4e
Kbb <78006318+KanBoorodom@users.noreply.github.com>
2021-05-20 17:21:23 +0700

    Update script.js

--- Frontendmentor/todo-app-main/script.js
+++ Frontendmentor/todo-app-main/script.js
@@ -1,50 +1,64 @@
-/* Swap theme */
+/* Swap theme------------------------------------------------------------- */
 const themeBtn = document.querySelector('.toggle_btn')
 const changeTheme = ()=>{
     document.body.classList.toggle('light')
 }
 themeBtn.addEventListener('click',changeTheme)
+/* End Swap theme ------------------------------------------------------------- */
+
+
+/* Create todo func------------------------------------------------------------- */
+const todoContainer = document.querySelector('.todo_container')
+const newTodo = (value)=>{
+    console.log(todoContainer)
+    const todo = document.createElement('div')
+    const todoText = document.createElement('p')
+    const todoCheckBox = document.createElement('input')
+    const todoCheckBoxLabel = document.createElement('label')
+    const todoCross = document.createElement('span')
+    todoText.textContent = value
+    todoCheckBox.type = "checkbox"
+    todoCheckBox.name = "checkbox"
+    todoCheckBoxLabel.htmlFor = "checkbox" /* sets or returns the value of the for attribute of a label. */
+    todoCross.classList.add('cross')
+    /* Cancle checked */
+    todoCheckBoxLabel.addEventListener('click',(e)=>{
+        if(!todoCheckBox.checked){
+            todoCheckBox.checked = true
+            todoCheckBoxLabel.classList.add('active')
+            todoText.classList.add('cross_active') 
+            console.log('hi')
+            return
+        }
+        todoCheckBox.checked = false
+        todoText.classList.remove('cross_active')
+        todoCheckBoxLabel.classList.remove('active')
+    })
+
+    /* delete done */
+    todoCross.addEventListener('click',(e)=>{
+        e.target.parentElement.remove()
+    })
+
+    todo.classList.add('todo')
+    todoCheckBoxLabel.classList.add('todo_circle')
+    todoCross.classList.add('cross')
+
+    todo.appendChild(todoCheckBox)
+    todo.appendChild(todoCheckBoxLabel)
+    todo.appendChild(todoText)
+    todo.appendChild(todoCross)
+    todoContainer.appendChild(todo)
+}
+/* End create todo func------------------------------------------------------------- */
 
 /* Todo */
 const todoInput = document.querySelector('#todo_input')
 const todos = []
 todoInput.addEventListener('keyup',function(e){
     if(e.key === 'Enter' || e.key === 13){
-        todos.push(e.target.value)
-       /*  console.log(todos) */
-    todoInput.value = ""
+        todos.push({value:e.target.value,checked:false})
+        newTodo(e.target.value)
+        todoInput.value = ""
     }
 })
-
-const newTodo = (value)=>{
-    const todo = document.createElement('div')
-    const todoText = document.createElement('p')
-    const todoCheckBox = document.createElement('input')
-    const todoCheckBoxLabel = document.createElement('label')
-    const todoCross = document.createElement('span')
-
-    todoText.textContent = value
-    todoCheckBox.type = "checkbox"
-    todoCheckBox.name = "checkbox"
-    todoCheckBoxLabel.htmlFor = "checkbox" /* sets or returns the value of the for attribute of a label. */
-    
-    todoCheckBoxLabel.addEventListener('click',(e)=>{
-        /* กดที่ label เพื่อยกเลิก check */
-        if(todoCheckBox.checked){
-            todoCheckBox.checked = false
-            todoText.style.textDecoration = "none"
-            todoCheckBoxLabel.classList.remove('active')
-        }else{
-            todoCheckBox.checked = false
-            todoText.style.textDecoration = "line-through"
-            todoCheckBoxLabel.classList.add('active')
-        }
-    })
-
-    todoCross.style.backgroundImage = "url('./images/icon-cross.svg')"
-    todoCross.addEventListener('click',(e)=>{
-        e.target.parentElement.remove()
-    })
-
-
-}
