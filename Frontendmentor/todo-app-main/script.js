4aa1fe6baa8dc358bf288739fc56a67554bf7ec4
Kbb <78006318+KanBoorodom@users.noreply.github.com>
2021-06-01 15:07:17 +0700

    add editable

--- Frontendmentor/todo-app-main/script.js
+++ Frontendmentor/todo-app-main/script.js
@@ -14,41 +14,106 @@ themeBtn.addEventListener('click',changeTheme)
 /* Create todo func------------------------------------------------------------- */
 const todoContainer = document.querySelector('.todo_container')
 const newTodo = (value)=>{
+    /* Create Component------------------------------------------------------------- */
     const todoDiv = document.createElement('div')
     const todoCircle = document.createElement('span')
     const todoLabel = document.createElement('label')
     const todoInput = document.createElement('input')
     const todoCross = document.createElement('span')
-/* 
-    const todo = document.createElement('div')
-    const todoText = document.createElement('p')
-    const todoCheckBox = document.createElement('input')
-    const todoCheckBoxLabel = document.createElement('label')
-    const todoCross = document.createElement('span')
-    todoText.textContent = value
-    todoCheckBox.type = "checkbox"
-    todoCheckBox.name = "checkbox"
-    todoCheckBoxLabel.htmlFor = "checkbox" /* link label with input field
-    todoCross.classList.add('cross')
- */   
+    const todoEdit = document.createElement('span')
+    const type = document.querySelector('.type_todo')
+
     todoContainer.appendChild(todoDiv)
     todoDiv.classList.add('todo')
     requestAnimationFrame(function () {
         todoDiv.classList.add('todo_create')
       })
+
     todoDiv.appendChild(todoCircle)
     todoCircle.classList.add('todo_circle')
     todoCircle.classList.add('hover_circle')
+
     todoDiv.appendChild(todoLabel)
     todoLabel.innerText = value
     todoLabel.appendChild(todoInput)
+
     todoInput.type = "checkbox"
+    todoInput.classList.add('todo_checkbox')
+
     todoDiv.appendChild(todoCross)
     todoCross.classList.add('cross')
 
-    const labels = [todoCircle,todoLabel]
-    labels.forEach((label)=>{
-        label.addEventListener('click',(e)=>{
+    todoDiv.appendChild(todoEdit)
+    todoEdit.classList.add('edit')
+    var editable = document.createAttribute('contenteditable')
+    editable.value = 'false'
+    todoLabel.setAttributeNode(editable)
+    /* End Create Component------------------------------------------------------------- */
+
+    /* check edit function------------------------------------------------------------- */
+    todoLabel.addEventListener('click',(e)=>{
+        e.preventDefault()
+    })
+    /* ลบข้อความทิ้งทั้งหมด */
+    const checkRemove = ()=>{
+        if(todoLabel.innerText.length === 0){
+            todoDiv.classList.add('todo_remove')    
+            setTimeout(function(){ 
+            todoCross.parentElement.remove()
+            },500)
+            return
+        }
+    }
+    /* แก้ข้อความบางส่วน */
+    const editSomeText = ()=>{
+        editable.value = 'false'
+        todoLabel.style.cursor = "default"
+        todoDiv.classList.remove('editTodo')
+    }
+    /* End check edit function------------------------------------------------------------- */
+
+    /* Start Edit------------------------------------------------------------- */
+    todoEdit.addEventListener('click',()=>{
+        /* ทำให้ที่ add กับ todo ตัวอื่นคลิ็กไม่ได้ */
+        todoDiv.classList.add('editTodo')
+        type.classList.add('nonclick')
+        todoLabel.style.cursor = "text"
+        const todos = document.querySelectorAll('.todo')
+        todos.forEach((todo)=>{
+            if(!todo.classList.contains('editTodo')){
+                todo.classList.toggle('blur')
+                todo.classList.toggle('nonclick')
+            }
+        })
+        /* ยืนยันการแก้ข้อความโดยกดที่ปุ่น enter*/
+        todoLabel.addEventListener('keypress',(e)=>{
+            if(e.key === 'Enter'){
+                e.preventDefault();
+                checkRemove()
+                type.classList.remove('nonclick')    
+                todos.forEach((todo)=>{
+                    if(!todo.classList.contains('editTodo')){
+                        todo.classList.remove('blur')
+                        todo.classList.remove('nonclick')
+                    }
+                })        
+                editSomeText()
+                return
+            }
+        })        
+        /* ยืนยันการแก้ข้อความโดยกดที่ปุ่น edit ซ้ำ */
+        if(editable.value == 'true'){
+            type.classList.remove('nonclick')
+            checkRemove()        
+            editSomeText()
+            return
+        } 
+        editable.value = 'true'
+    })
+    /* End Edit------------------------------------------------------------- */
+
+    /* Checkbox------------------------------------------------------------- */
+    todoCircle.addEventListener('click',(e)=>{
             /* checkbox is not checked */
             if(!todoInput.checked){
                 todoInput.checked = true
@@ -64,30 +129,23 @@ const newTodo = (value)=>{
             todoCircle.classList.remove('circle_active')
             todoCircle.classList.add('hover_circle')
         })
-    })
-/* 
-    /* delete done 
-    todoCross.addEventListener('click',(e)=>{
-        e.target.parentElement.remove()
-    })
+    /* End Checkbox------------------------------------------------------------- */
 
-    todo.classList.add('todo')
-    todoCheckBoxLabel.classList.add('todo_circle')
-    todoCross.classList.add('cross')
-
-    todo.appendChild(todoCheckBox)
-    todo.appendChild(todoCheckBoxLabel)
-    todo.appendChild(todoText)
-    todo.appendChild(todoCross)
-    todoContainer.appendChild(todo)
- */
+    /* Delete------------------------------------------------------------- */
     todoCross.addEventListener('click',()=>{
         todoDiv.classList.add('todo_remove')    
+        const todos = document.querySelectorAll('.todo')
+        todos.forEach((todo)=>{
+            todo.classList.remove('blur')
+            todo.classList.remove('nonclick')
+        })
+        type.classList.remove('nonclick')
         setTimeout(function(){ 
             todoCross.parentElement.remove()
             },500
         )
     })
+    /* End Delete------------------------------------------------------------- */
 }
 /* End create todo func------------------------------------------------------------- */
 
@@ -95,7 +153,7 @@ const newTodo = (value)=>{
 const todoInput = document.querySelector('#todo_input')
 const todos = []
 todoInput.addEventListener('keyup',function(e){
-    if(e.key === 'Enter' || e.key === 13){
+    if((e.key === 'Enter' || e.key === 13) && (e.target.value.length>0)){
         todos.push({value:e.target.value,checked:false})
         newTodo(e.target.value)
         todoInput.value = ""
