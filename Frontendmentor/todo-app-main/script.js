5d292557b5831b45c62389c7375aa5c9af2ccb13
Kbb <78006318+KanBoorodom@users.noreply.github.com>
2021-06-07 23:09:46 +0700

    drag and drop add

--- Frontendmentor/todo-app-main/script.js
+++ Frontendmentor/todo-app-main/script.js
@@ -1,5 +1,21 @@
-
-
+const todoContainer = document.querySelector('.todo_container')
+const todoInfo = document.querySelector('.todo_info')
+const status = document.querySelector('.status')
+const clear = document.querySelector('.clear')
+const widthCheck = window.matchMedia("(max-width:800px)")
+function checkStatusPosition(width){
+    if(width.matches){
+        todoContainer.appendChild(status)
+        status.classList.add('status_bot')
+    }
+    else{
+        todoInfo.insertBefore(status,clear)
+        status.classList.remove('status_bot')
+    }
+}
+widthCheck.addListener(checkStatusPosition)
+checkStatusPosition(widthCheck)
+window.addEventListener('resize',checkStatusPosition(widthCheck))
 window.onload = (event)=>{
     document.querySelector('body').classList.remove('preload')
 }
@@ -12,20 +28,41 @@ const changeTheme = ()=>{
 themeBtn.addEventListener('click',changeTheme)
 /* End Swap theme ------------------------------------------------------------- */
 
+/* Take input and create todo */
+const todoInput = document.querySelector('#todo_input')
+//const todoInfo = document.querySelector('.todo_info')
+todoInput.addEventListener('keyup',function(e){
+    const todoSize = document.querySelectorAll('.todo').length
+    if((e.key === 'Enter' || e.key === 13) && (e.target.value.length>0)){
+        newTodo(e.target.value)
+        checkTodoSize()
+        todoInput.value = ""
+    }
+})
+/* End Take input and create todo ------------------------------------------------------------- */
 
 /* Create todo func------------------------------------------------------------- */
-const todoContainer = document.querySelector('.todo_container')
+
+//const todoContainer = document.querySelector('.todo_container')
+
+var todoSize
+var itemLeft = document.querySelector('.item_left')
+
 function checkTodoSize(){
-    const todoSize = document.querySelectorAll('.todo').length
-    if(todoSize > 0){
-        todoInfo.classList.add('show_info')        
+    todoSize = document.querySelectorAll('.todo').length
+    const activeCircle = document.querySelectorAll('.circle_active').length
+    if(todoSize > 0 && (!todoInfo.classList.contains('show'))){
+        todoInfo.classList.add('show') 
+        status.classList.add('show')       
     }   
     else if(todoSize == 0){
-        todoInfo.classList.remove('show_info')
+        todoInfo.classList.remove('show')
+        status.classList.remove('show')   
     }
-    document.querySelector('.item_left').innerText = `${todoSize} items left`
-}
 
+    itemLeft.innerText = `${todoSize-activeCircle} items left`
+}
+var dragSrcEl = ''
 const newTodo = (value)=>{
     /* Create Component------------------------------------------------------------- */
     const todoDiv = document.createElement('div')
@@ -62,6 +99,10 @@ const newTodo = (value)=>{
     var editable = document.createAttribute('contenteditable')
     editable.value = 'false'
     todoLabel.setAttributeNode(editable)
+
+    var draggable = document.createAttribute('draggable')
+    draggable.value = true
+    todoLabel.setAttributeNode(draggable)
     /* End Create Component------------------------------------------------------------- */
 
     /* check edit function------------------------------------------------------------- */
@@ -82,13 +123,13 @@ const newTodo = (value)=>{
     const editSomeText = ()=>{
         type.classList.remove('nonclick')    
         editable.value = 'false'
-        todoLabel.style.cursor = "default"
+        todoLabel.style.cursor = "move"
         todoDiv.classList.remove('editTodo')
     }
     /* End check edit function------------------------------------------------------------- */
 
     /* Start Edit------------------------------------------------------------- */
-    todoEdit.addEventListener('click',(e)=>{
+    todoEdit.addEventListener('click',()=>{
         todoDiv.classList.add('editTodo')
         todoLabel.style.cursor = "text"
 
@@ -131,21 +172,25 @@ const newTodo = (value)=>{
     /* End Edit------------------------------------------------------------- */
 
     /* Checkbox------------------------------------------------------------- */
-    todoCircle.addEventListener('click',(e)=>{
+    todoCircle.addEventListener('click',()=>{
+        console.log('clciked')
             /* checkbox is not checked */
             if(!todoInput.checked){
                 todoInput.checked = true
                 todoCircle.classList.remove('hover_circle')
                 todoLabel.classList.add('cross_active') 
                 todoCircle.classList.add('circle_active')
+                checkTodoSize()         
                 return
             }
             /* check box is checked */
+            checkTodo = document.querySelectorAll('.circle_active').length
             todoInput.checked = false
             todoLabel.classList.remove('cross_active')
             todoLabel.classList.remove('active')
             todoCircle.classList.remove('circle_active')
             todoCircle.classList.add('hover_circle')
+            checkTodoSize()
         })
     /* End Checkbox------------------------------------------------------------- */
 
@@ -164,28 +209,96 @@ const newTodo = (value)=>{
             },500
         )
         checkTodoSize()
-    })
+    },false)
     /* End Delete------------------------------------------------------------- */
+
+    /* Drag------------------------------------------------------------- */
+    
+    todoLabel.addEventListener('dragstart',(e)=>{
+        console.log('hi')
+        todoDiv.style.opacity = '0.4'
+        dragSrcEl = todoLabel
+        console.log(dragSrcEl)
+        e.dataTransfer.effectAllowed = 'move'
+        e.dataTransfer.setData('text',todoDiv.innerText)
+    },false)
+    
+    todoLabel.addEventListener('dragend',()=>{
+        todoDiv.style.opacity = '1.0'     
+    },false)
+    todoLabel.addEventListener('dragover',(e)=>{
+        e.preventDefault()
+        todoDiv.style.transform = 'scale(1.03,1.03)' 
+    },false)
+    todoLabel.addEventListener('dragleave',()=>{
+        todoDiv.style.transform = 'scale(1,1)' 
+    },false)
+    todoLabel.addEventListener('drop',(e)=>{
+        todoDiv.style.transform = 'scale(1,1)'
+        dragSrcEl.innerText = todoLabel.innerText
+        todoLabel.innerText = e.dataTransfer.getData('text')
+    },false)
+    /* End Drag------------------------------------------------------------- */
 }
 /* End create todo func------------------------------------------------------------- */
 
-/* Todo */
-const todoInput = document.querySelector('#todo_input')
-const todoInfo = document.querySelector('.todo_info')
-/* const todos = []
- */
-todoInput.addEventListener('keyup',function(e){
-    const todoSize = document.querySelectorAll('.todo').length
-    if((e.key === 'Enter' || e.key === 13) && (e.target.value.length>0)){
-/*         if(todoSize == 0){
-            todoInfo.classList.add('show_info')           
-        }     */
-       /*  todos.push({value:e.target.value,checked:false}) */
-        newTodo(e.target.value)
-        /*  */
-        checkTodoSize()
-        todoInput.value = ""
+/* All Active Completed------------------------------------------------------------- */
+var alreadyActive 
+status.addEventListener('click',(e)=>{
+    var alreadyActive = document.querySelector('.status_active')
+    var checks
+    if(!e.target.classList.contains('status')){
+        alreadyActive.classList.remove('status_active')
+        e.target.classList.add('status_active')
+
+        if(e.target.innerText === 'Active'){
+            checks = document.querySelectorAll('.todo_circle')
+            checks.forEach(check=>{
+                if(check.classList.contains('circle_active')){
+                    check.parentElement.classList.add('hide')
+                }
+                else{
+                    check.parentElement.classList.remove('hide')
+                }
+            })
+        }
+        else if(e.target.innerText === 'Completed'){
+            checks = document.querySelectorAll('.todo_circle')
+            checks.forEach(check=>{
+                if(!check.classList.contains('circle_active')){
+                    check.parentElement.classList.add('hide')
+                }
+                else{
+                    check.parentElement.classList.remove('hide')
+                }
+            })
+        }
+        else{
+            checks = document.querySelectorAll('.hide')
+            checks.forEach(check=>{
+                check.classList.remove('hide')
+            }) 
+        }
+
     }
 })
+/* End All Active Completed------------------------------------------------------------- */
 
+/* Clear------------------------------------------------------------- */
+clear.addEventListener('click',()=>{
+    const checks = document.querySelectorAll('.circle_active')
+    checks.forEach(check=>{
+        check.parentElement.classList.add('todo_remove')
+        setTimeout(function(){ 
+            check.parentElement.remove()
+            todoSize = document.querySelectorAll('.todo').length
+            if(todoSize === 0){
+                todoInfo.classList.remove('show')
+                status.classList.remove('show') 
+            } 
+        },500)
+    })
+   
+
+})
 
