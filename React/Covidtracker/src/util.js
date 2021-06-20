955992c41e655790cd5057e9fdb9947611ff6ce1
Kbb <78006318+KanBoorodom@users.noreply.github.com>
2021-06-20 14:34:43 +0700

    Add files via upload

---
+++ React/Covidtracker/src/util.js
@@ -0,0 +1,15 @@
+export const sortData = data => {
+    const sortedData = [...data]
+    return sortedData.sort((a,b) => a.cases>b.cases ? -1 :1)
+}
+/* 
+sortedData.sort((a,b)=>{
+    if(a.cases > b.cases){
+        return -1 //-1 = false แต่ถ้าใช้ false จะไม่ sort
+    }
+    else{
+        return 1 //1 = true
+    }
+})
+return sortedData
+ */
